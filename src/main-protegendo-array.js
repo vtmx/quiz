9f185b1f762ba21e039c4fbacd38f000263a4c1b
vtmx <vtmx22@gmail.com>
2023-04-04 21:37:36 -0300

    update shuffleArray com a ajuda do chatgpt

--- src/main-protegendo-array.js
+++ src/main-protegendo-array.js
@@ -2,10 +2,10 @@ import './main.css';
 import data from './az-900.json';
 
 class Quiz {
-  titleQuiz = data['title'];
   questions = data['questions'];
-  questionsCount = 1;
+  questionsCount = 0;
   maxQuestions = 20;
+  isLastQuestion = false;
 
   shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -16,13 +16,31 @@ class Quiz {
   }
 
   randomizeQuestionsAndAnswers() {
-    for (let i = 0; i < this.questions.length; i++) {
-      const question = this.questions[i];
+    const randomQuestions = [...this.questions];
+
+    for (let i = 0; i < randomQuestions.length; i++) {
+      const question = randomQuestions[i];
       this.shuffleArray(question.answers);
     }
-    this.shuffleArray(this.questions);
-    return this.questions;
+
+    this.shuffleArray(randomQuestions);
+
+    return randomQuestions;
   }
+
+  // shuffleArray(array) {
+  //   let tempArray = [...array];
+  //   let suffleArray = [];
+  //   let randomItem = 0;
+
+  //   for (let i = 0; i < array.length; i++) {
+  //     randomItem = Math.floor(Math.random() * tempArray.length);
+  //     suffleArray.push(tempArray[randomItem]);
+  //     tempArray.splice(randomItem, 1);
+  //   }
+
+  //   return suffleArray;
+  // }
 }
 
 const quiz = new Quiz();
