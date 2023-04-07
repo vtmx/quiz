8b7adc8f8f40540b4df58dadbde83fcca0a32d54
vtmx <vtmx22@gmail.com>
2023-04-07 15:55:55 -0300

    update js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,34 @@ import './main.css';
 import data from './az-900.json';
 
 class Quiz {
-  titleQuiz = data['title'];
-  questions = data['questions'];
-  questionsCount = 1;
-  maxQuestions = 20;
+  constructor(amountQuestions = 20) {
+    this.title = data['title'];
+    this.questions = data['questions'];
+    this.randomQuestionsAndAnswers = this.randomizeQuestionsAndAnswers();
+    this.randomQuestions = this.randomizeQuestions();
+    this.randomAnswers = this.randomizeAnswers();
+    this.amountQuestions = amountQuestions;
+    this.currentQuestionNumber = 3;
+    this.lastQuestionNumber = 10;
+    this.questionsCount = 1;
+  }
+
+  init() {
+    const anwsersRadios = document.querySelectorAll('.anwsers input[type="radio"]');
+
+    anwsersRadios.forEach(element => {
+      element.addEventListener('click', event => {
+        this.uncheckRadios(anwsersRadios);
+        event.target.checked = true;
+      });
+    });
+  }
+
+  uncheckRadios(elements) {
+    elements.forEach(element => {
+      element.checked = true;
+    });
+  }
 
   shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -29,17 +53,53 @@ class Quiz {
   }
 
   randomizeQuestionsAndAnswers() {
-    for (let i = 0; i < this.questions.length; i++) {
-      const question = this.questions[i];
+    const randomQuestions = [...this.questions];
+    for (let i = 0; i < randomQuestions.length; i++) {
+      const question = randomQuestions[i];
       this.shuffleArray(question.answers);
     }
-    this.shuffleArray(this.questions);
-    return this.questions;
+    this.shuffleArray(randomQuestions);
+    return randomQuestions;
+  }
+
+  render(component) {
+    document.querySelector('#app').insertAdjacentHTML('afterbegin', component);
   }
 }
 
 const quiz = new Quiz();
-// console.log(data.title);
-// console.log(data['questions'][0]['question']);
-quiz.randomizeQuestionsAndAnswers();
-console.log(quiz.questions);
+
+quiz.render(`
+<div id="quiz">
+  <h1>${quiz.title}</h1>
+
+  <div id="status">
+    <div id="progress"></div>
+    <div id="count"><span class="current">${quiz.currentQuestionNumber}</span> de <span class="max">${quiz.lastQuestionNumber}</span></div>
+    <div id="timer">30:01</div>
+  </div>
+
+  <div id="question-1" class="question">
+    <h2>Qual é o cavaleiro de pégaso?</h2>
+    <div class="anwsers">
+      <label class="anwser"><input type="radio" id="anwser-1" name="anwser-1" /><span>Ikki</span></label>
+      <label class="anwser"><input type="radio" id="anwser-2" name="anwser-1" /><span>Seiya</span></label>
+      <label class="anser"><input type="radio" id="anwser-3" name="anwser-1" /><span>Shiriyu</span></label>
+    </div>
+  </div>
+
+  <!-- <details class="response" hidden open> -->
+  <details class="response" open>
+    <summary tabindex="3">Resposta</summary>
+    <div class="explain"><p>Seiya recebeu a armadura na Grécia.</p></div>
+  </details>
+
+  <div id="controls">
+    <input type="button" class="prev" value="&laquo; Anterior" />
+    <input type="button" class="next" value="Próxima &raquo;" />
+  </div> <!-- #controls -->
+</div> <!-- #quiz -->`
+);
+
+quiz.init();
+console.log(quiz.randomQuestionsAndAnswers);
